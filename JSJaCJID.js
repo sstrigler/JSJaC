ffa067ba4942e4215e38346e9dba736b504c5e11
Stefan Strigler <stefan@strigler.de>
2007-06-18 11:34:52 +0000

    JSJaCJJID: JSJaCJIDInvalidException added and used throughout where applicable

--- JSJaCJID.js
+++ JSJaCJID.js
@@ -1,49 +1,87 @@
+// list of forbidden chars for nodenames
+var JSJACJID_FORBIDDEN = ['"',' ','&','\'','/',':','<','>','@']; 
+
 function JSJaCJID(jid) {
-  if (jid.node && jid.domain && jid.resource) {
-    this._node = jid.node;
-    this._domain = jid.domain;
-    this._resource = jid.resource;
-  } else {
-    this._node = jid.substring(0,jid.indexOf('@'));
-    if (jid.indexOf('/') != -1) {
-      this._domain = jid.substring(jid.indexOf('@')+1,jid.indexOf('/'));
-      this._resource = jid.substring(jid.indexOf('/')+1);
-    } else {
-      this._domain = jid.substring(jid.indexOf('@')+1);
-      this._resource = "";
-    }
-  }
+
+  // getters
 
   this.getNode = function() { return this._node; };
   this.getDomain = function() { return this._domain; };
   this.getResource = function() { return this._resource; };
 
   this.setNode = function(node) {
+    this._checkNodeName(node);
     this._node = node;
     return this;
-  }
+  };
+
+  // setters
 
   this.setDomain = function(domain) {
+    if (!domain || domain == '')
+      throw new JSJaCJIDInvalidException("domain name missing");
+    // chars forbidden for a node are not allowed in domain names
+    // anyway, so let's check
+    this._checkNodeName(domain); 
     this._domain = domain;
     return this;
-  }
+  };
 
   this.setResource = function(resource) {
-    resource = resource || "";
     this._resource = resource;
     return this;
-  }
+  };
+
+  // public functions
 
   this.toString = function() {
-    if (this.getResource() != "")
-      return this.getNode() + '@' + 
-        this.getDomain() + '/' + 
-        this.getResource();
-    else
-      return this.getNode() + '@' + this.getDomain();
+    var jid = '';
+    if (this.getNode() && this.getNode() != '')
+      jid = this.getNode() + '@';
+    jid += this.getDomain(); // we always have a domain
+    if (this.getResource() && this.getResource() != "")
+      jid += '/' + this.getResource();
+    return jid;
   }
 
   this.removeResource = function() {
-    this.setResource("");
+    this.setResource();
+  };
+
+
+  // private functions
+
+  // throws exception on error
+  this._checkNodeName = function(nodeprep) {
+    for (var i=0; i< JSJACJID_FORBIDDEN.length; i++) {
+      if (nodeprep.indexOf(JSJACJID_FORBIDDEN[i]) != -1) {
+        throw new JSJaCJIDInvalidException("forbidden char in nodename: "+JSJACJID_FORBIDDEN[i]);
+      }
+    }
+  };
+
+
+  // constructor
+
+  if (typeof(jid) == 'string') {
+    if (jid.indexOf('@') != -1) {
+        this.setNode(jid.substring(0,jid.indexOf('@')));
+        jid = jid.substring(jid.indexOf('@')+1);
+    }
+    if (jid.indexOf('/') != -1) {
+      this.setResource(jid.substring(jid.indexOf('/')+1));
+      jid = jid.substring(0,jid.indexOf('/'));
+    } 
+    this.setDomain(jid);
+  } else {
+    this.setNode(jid.node);
+    this.setDomain(jid.domain);
+    this.setResource(jid.resource);
   }
+
+}
+
+function JSJaCJIDInvalidException(msg) {
+  this.message = msg;
+  this.name = "JSJaCJIDInvalidException";
 }
