f8a019b4264981ef71287ca864860dd9255c7bf6
Stefan Strigler <stefan@strigler.de>
2007-07-04 09:12:45 +0000

    more levels default level check for console being defined

--- src/FirebugLogger.js
+++ src/FirebugLogger.js
@@ -5,13 +5,25 @@ function FirebugLogger(level) {
   // api compat
   this.start = function() {};
   this.log = function(msg, level) {
+    level = level || 0;
     if (level > this.level)
       return;
+    if (typeof(console) == 'undefined') 
+      return;
     switch (level) {
-      case 1:
+    case 0:
+      console.warn(msg);
+      break;
+    case 1:
       console.error(msg);
       break;
-      default:
+    case 2:
+      console.info(msg);
+      break;
+    case 4:
+      console.debug(msg);
+      break;
+    default:
       console.log(msg);
       break;
     }
