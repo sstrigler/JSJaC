320ba65015ce2114dcc6123072149673d08d410a
Stefan Strigler <stefan@strigler.de>
2007-08-16 14:58:05 +0000

    _suspend: support using 'pause' attribute for suspending _resume: support using 'pause' attribute when resuming

--- src/JSJaCHttpBindingConnection.js
+++ src/JSJaCHttpBindingConnection.js
@@ -8,7 +8,7 @@ var JSJACHBC_BOSH_VERSION = "1.6";
 var JSJACHBC_MAX_HOLD = 1;
 var JSJACHBC_MAX_WAIT = 300; 
 
-
+var JSJACHBC_MAXPAUSE = 120;
 /**
  * Instantiates an HTTP Binding session
  * @class Implementation of {@link
@@ -45,6 +45,11 @@ function JSJaCHttpBindingConnection(oArg) {
    * @private
    */
   this._min_polling = 0;
+
+  /**
+   * @private
+   */
+  this._pause = 0;
   /**
    * @private
    */
@@ -99,19 +104,21 @@ function JSJaCHttpBindingConnection(oArg) {
    * @private
    */
   this._getSuspendVars = function() {
-    return ('host,port,secure,_rid,_last_rid,_wait,_min_polling,_inactivity,_hold,_last_requests').split(',');
+    return ('host,port,secure,_rid,_last_rid,_wait,_min_polling,_inactivity,_hold,_last_requests,_pause').split(',');
   }
   this._handleInitialResponse = JSJaCHBCHandleInitialResponse;
   this._prepareResponse = JSJaCHBCPrepareResponse;
+  this._reInitStream = JSJaCHBCReInitStream;
   /**
    * @private
    */
   this._resume = function() { 
     /* make sure to repeat last request as we can be sure that
-     * it had failed 
+     * it had failed (only if we're not using the 'pause' attribute
      */
-    if (this._rid >= this._last_rid)
-      this._rid = this._last_rid-1; 
+    if (this._pause == 0 && this._rid >= this._last_rid)
+        this._rid = this._last_rid-1; 
+
     this._process();
     this._inQto = setInterval("oCon._checkInQ();",JSJAC_CHECKINQUEUEINTERVAL);
     this._interval= setInterval("oCon._checkQueue()",JSJAC_CHECKQUEUEINTERVAL);
@@ -128,8 +135,43 @@ function JSJaCHttpBindingConnection(oArg) {
     return this._hold;
   };
   this._setupRequest = JSJaCHBCSetupRequest;
+  /**
+   * @private
+   */
+  this._suspend = function() {
+    if (this._pause == 0)
+      return; // got nothing to do
 
-  this._reInitStream = JSJaCHBCReInitStream;
+    var slot = this._getFreeSlot();
+    // Intentionally synchronous
+    this._req[slot] = this._setupRequest(false);
+
+    var reqstr = "<body pause='"+this._pause+"' xmlns='http://jabber.org/protocol/httpbind' sid='"+this._sid+"' rid='"+this._rid+"'";
+    if (JSJAC_HAVEKEYS) {
+      reqstr += " key='"+this._keys.getKey()+"'";
+      if (this._keys.lastKey()) {
+        this._keys = new JSJaCKeys(hex_sha1,this.oDbg);
+        reqstr += " newkey='"+this._keys.getKey()+"'";
+      }
+
+    }
+    reqstr += ">";
+
+    while (this._pQueue.length) {
+      var curNode = this._pQueue[0];
+      reqstr += curNode;
+      this._pQueue = this._pQueue.slice(1,this._pQueue.length);
+    }
+
+    //reqstr += "<presence type='unavailable' xmlns='jabber:client'/>";
+    reqstr += "</body>";
+
+    // Wait for response (for a limited time, 5s)
+    var abortTimerID = setTimeout("oCon._req["+slot+"].r.abort();", 5000);
+    this.oDbg.log("Disconnecting: " + reqstr,4);
+    this._req[slot].r.send(reqstr);
+    clearTimeout(abortTimerID); 
+  }
 }
 
 /**
@@ -266,6 +308,12 @@ function JSJaCHBCHandleInitialResponse(slot) {
     this._setHold(body.getAttribute('requests')-1);
   this.oDbg.log("set hold to " + this._getHold(),2);
 
+  if (body.getAttribute('ver'))
+    this._bosh_version = body.getAttribute('ver');
+
+  if (body.getAttribute('maxpause'))
+    this._pause = Number.max(body.getAttribute('maxpause'), JSJACHBC_MAXPAUSE);
+
   // must be done after response attributes have been collected
   this.setPollInterval(this._timerval);
 
@@ -392,7 +440,7 @@ function JSJaCHBCDisconnect() {
   reqstr += "</body>";
 
   // Wait for response (for a limited time, 5s)
-  var abortTimerID = setTimeout("this._req[slot].abort();", 5000);
+  var abortTimerID = setTimeout("oCon._req["+slot+"].r.abort();", 5000);
   this.oDbg.log("Disconnecting: " + reqstr,4);
   this._req[slot].r.send(reqstr);	
   clearTimeout(abortTimerID);
