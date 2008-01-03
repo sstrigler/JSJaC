74fbcf272753bcd3aa2c021e010fbd86f101607c
Stefan Strigler <stefan@strigler.de>
2008-01-03 14:30:45 +0000

    added link to jsjac's homepage

--- src/header.js
+++ src/header.js
@@ -14,4 +14,6 @@
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * Please visit http://zeank.in-berlin.de/jsjac/ for details about JSJaC.
  */
