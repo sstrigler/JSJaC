51df27699befc2b37a3ff5b49f85d390905e32a9
Stefan Strigler <stefan@strigler.de>
2008-02-07 13:25:33 +0000

    0002-Sometimes-polling-connection-return-empty-results-h.patch

--- src/JSJaCHttpPollingConnection.js
+++ src/JSJaCHttpPollingConnection.js
@@ -190,13 +190,18 @@ JSJaCHttpPollingConnection.prototype._parseResponse = function(r) {
   }
 
   this.oDbg.log(req.getAllResponseHeaders(),4);
-  var aPList = req.getResponseHeader('Set-Cookie');
-  aPList = aPList.split(";");
-  var sid;
-  for (var i=0;i<aPList.length;i++) {
-    var aArg = aPList[i].split("=");
-    if (aArg[0] == 'ID')
-      sid = aArg[1];
+  var sid, aPList = req.getResponseHeader('Set-Cookie');
+
+  if (aPList == null)
+    sid = "-1:0"; // Generate internal server error
+  else {
+    aPList = aPList.split(";");
+    var sid;
+    for (var i=0;i<aPList.length;i++) {
+      var aArg = aPList[i].split("=");
+      if (aArg[0] == 'ID')
+        sid = aArg[1];
+    }
   }
 
   // http polling component error
