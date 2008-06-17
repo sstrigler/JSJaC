3e875c47df2f55493a1984247ce9b4d0ad95372c
Stefan Strigler <stefan@strigler.de>
2008-06-17 09:25:54 +0000

    tagged as v1.3.1

--- src/header.js
+++ src/header.js
@@ -1,19 +1,10 @@
 /* JSJaC - The JavaScript Jabber Client Library
- * Copyright (C) 2004-2007 Stefan Strigler
+ * Copyright (C) 2004-2008 Stefan Strigler
  *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ * JSJaC is licensed under the terms of the Mozilla Public License
+ * version 1.1 or, at your option, under the terms of the GNU General
+ * Public License version 2 or subsequent, or the terms of the GNU Lesser
+ * General Public License version 2.1 or subsequent. 
  *
  * Please visit http://zeank.in-berlin.de/jsjac/ for details about JSJaC.
  */
