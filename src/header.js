bed823976cd49404d1fadb34b195da153f6c86df
Stefan Strigler <stefan.strigler@erlang-solutions.com>
2014-02-02 14:21:04 +0100

    update header

--- src/header.js
+++ src/header.js
@@ -1,10 +1,10 @@
 /* JSJaC - The JavaScript Jabber Client Library
- * Copyright (C) 2004-2008 Stefan Strigler
+ * Copyright (C) 2004-20014 Stefan Strigler et al.
  *
  * JSJaC is licensed under the terms of the Mozilla Public License
  * version 1.1 or, at your option, under the terms of the GNU General
  * Public License version 2 or subsequent, or the terms of the GNU Lesser
  * General Public License version 2.1 or subsequent. 
  *
- * Please visit http://zeank.in-berlin.de/jsjac/ for details about JSJaC.
+ * Please visit https://github.com/sstrigler/JSJaC/ for details about JSJaC.
  */
